926915b05f06b23962dbb6a50cc1260fbba057e2
Aadit Kamat <30969577+aaditkamat@users.noreply.github.com>
2023-06-01 15:56:30 -0400

    Update SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,16 @@
-import { DocSearch } from '@docsearch/react';
-
+import docSearch  from '@docsearch/react';
+const { DocSearch } = docSearch;
+ 
 import '@docsearch/css';
 
 const SearchBar = () => {
-  return <DocSearch appId="YOUR_APP_ID" indexName="YOUR_INDEX_NAME" apiKey="YOUR_SEARCH_API_KEY" />
+  return (
+    <DocSearch
+      appId="O4EM35CRG4"
+      indexName="netlify_3c36968e-e94f-44df-8738-07d71850ba64_main_all"
+      apiKey="c173b146b3d74fa5869b9e5f2b7cd5b0"
+    />
+  );
 };
 
 export default SearchBar;
