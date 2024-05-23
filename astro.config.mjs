66fa6ad008c6c23e46651e6a70a600aa6706c35d
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2024-05-23 10:34:58 +0000

    Remove imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
-import icon from 'astro-icon';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
