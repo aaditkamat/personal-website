0437af56c54bfed75cb848fd8950cdc5dab46da2
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2024-12-08 15:47:28 -0500

    Add URLs corresponding to different sections of my resume

--- src/data.ts
+++ src/data.ts
@@ -97,10 +97,11 @@ export const podcastData = [
 export const timelineData = [
   {
     title: "January 2023 - December 2024",
-    cardTitle: "University of Florida, Warringtom College of Business",
+    cardTitle: "University of Florida, Warrington College of Business",
     cardSubtitle: "Master's in Information Systems and Operations Management",
     cardDetailedText:
       "As a graduate student under the Data Science track, I learnt how to apply data science techniques to solve real-world business problems. I took courses on databases, data mining, and business intelligence. I also worked on consulting projects with non profit organizations to help them improve their operations and make data-driven decisions",
+    url: "https://warrington.ufl.edu/master-of-science-in-information-systems-and-operations-management/"
   },
   {
     title: "June 2024 - August 2024",
@@ -108,6 +109,7 @@ export const timelineData = [
     cardSubtitle: "Data Engineering Intern",
     cardDetailedText:
       "I teamed up with senior analysts to create a customized 13-week cash flow model, which improved our forecasting accuracy and helped with strategic planning. I also put together a detailed data dictionary for an Excel model to improve data consistency and accessibility",
+    url: "https://getzlerhenrich.com"
   },
   {
     title: "March 2022 - September 2022",
@@ -115,61 +117,70 @@ export const timelineData = [
     cardSubtitle: "Software Engineer",
     cardDetailedText:
       "In addition to creating automated data pipelines that bring Visa financial data right into mobile apps, I rolled out functional unit tests to ensure the Visa developer portal is accessible and user-friendly as well as designed and document RESTful APIs for the payment microservices",
+    url: "https://www.visa.com.sg/about-visa/global-innovation-centers/singapore.html"
   },
   {
     title: "May 2021 - March 2022",
     cardTitle: "Shopee Singapore Pte Ltd",
     cardSubtitle: "Quality Assurance Engineer",
     cardDetailedText:
-      "I helped develop and maintain over 100 test cases for our Promotion modules, significantly enhancing the user experience for more than a million customers. I also worked closely with QA Engineers from ten different departments to refresh our training syllabus to guide new engineers in automation tools like Espresso and UiAutomator.",
+      "I helped develop and maintain over 100 test cases for the Promotion modules (Flash Sale/Bundle Deal/Add on Deal/Discount) for the Android App, significantly enhancing the user experience for more than a million customers. I also worked closely with QA Engineers from other departments to refresh our training syllabus and guide new engineers in automation tools like Espresso and UiAutomator.",
+    url: "https://seller.shopee.sg/edu/article/20254/types-of-promotions"
   },
   {
     title: "December 2020 - March 2021",
     cardTitle: "Atomionics Pte Ltd",
     cardSubtitle: "Software Engineering Intern",
-    cardDetailedText: "
-      "I collaborated with the senior physicists to build solutions enabling them to run cold atom interferometry experiments using sensors more efficiently"
+    cardDetailedText:
+      "I collaborated with the senior physicists to build solutions enabling them to run cold atom interferometry experiments for their quantum gravimeter using Field Programmable Gate Array (FPGA) hardware more efficiently",
+    url: "https://www.atomionics.com/our-solution"
   },
   {
     title: "May 2020 - August 2020",
     cardTitle: "National University of Singapore (NUS) Information Technology",
     cardSubtitle: "Software Engineering Intern",
     cardDetailedText:
-      "I designed a web portal that would make it easier for network administrators in NUS IT to review firewall rules on a regular basis"
+      "I designed a web portal that would make it easier for network administrators in NUS IT to review firewall rules on a regular basis",
+    url: "https://nusit.nus.edu.sg/support/faqpage/firewall-requests-faq/"
   },
   {
     title: "May 2019 - October 2019",
     cardTitle: "Autodesk Asia Pte Ltd",
     cardSubtitle: "Test Automation Intern",
     cardDetailedText:
-      "I developed automation scripts in Python to reduce manual effort required for regression testing, performed cross platform testing for different versions of Autodesk products, as well as devised a merge plan to combine common functionalities across test codebases"
+      "I developed automation scripts in Python to reduce manual effort required for regression testing, performed cross platform testing for different versions of Autodesk products, as well as devised a merge plan to combine common functionalities across test codebases",
+    url: "https://www.autodesk.com/products"
   },
   {
     title: "May 2019 - September 2019",
     cardTitle: "National University of Singapore (NUS)",
     cardSubtitle: "CP2106B Orbital Advisor",
     cardDetailedText:
-      "I guided 5 teams to successfully complete their projects as part of the Orbital program, which is the NUS School of Computing's self-driven programming summer experience designed to give first year students flavour of what a real life software development project looks like"
+      "I guided 5 teams to successfully complete their projects as part of the Orbital program, which is the NUS School of Computing's self-driven programming summer experience designed to give first year students flavour of what a real life software development project looks like",
+    url: "https://nusskylab-dev.comp.nus.edu.sg"
   },
   {
     title: "May 2019 - September 2019",
     cardTitle: "National University of Singapore (NUS)",
     cardSubtitle: "Web Developer Intern",
     cardDetailedText:
-      "I enhanced the Skylab platform so as to ensure a smoother, more seamless experience for students using the Learning Management System (LMS) platform for their course submissions"
+      "I enhanced the Skylab platform so as to ensure a smoother, more seamless experience for students using the Learning Management System (LMS) platform for their course submissions",
+    url: "https://nusskylab-dev.comp.nus.edu.sg/public_views/public_staff"
   },
   {
     title: "December 2018 - March 2019",
     cardTitle: "Learnseeker AI",
     cardSubtitle: "Software Engineering Intern",
     cardDetailedText:
-      "I created an SMS dashboard to track leads generated and a Marketing Dashboard for easier customer segmentation. I also redesigned the internal Customer Relationship Management (CRM) software to provide better insight into customer retention data"
-  }
+      "I created an SMS dashboard to track leads generated and a Marketing Dashboard for easier customer segmentation. I also redesigned the internal Customer Relationship Management (CRM) software to provide better insight into customer retention data",
+    url: "https://learnseeker.com"
+  },
   {
     title: "August 2017 - May 2021",
     cardTitle: "National University of Singapore (NUS)",
     cardSubtitle: "Bachelor of Computing in Computer Science",
     cardDetailedText:
       "This program gave me a foundation for all my technical explorations. I not only learnt software engineering skills but also the fundamentals underlying computer systems like OS, Computer Architecture in addition to taking electives in AI and Machine Learning.",
+    url: "https://www.comp.nus.edu.sg"
   },
 ];
