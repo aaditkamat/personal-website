b376f4a19f9334a35d7d05373f24bff94a2e93ce
Aadit Kamat <>
2025-05-26 11:43:18 +0800

    Add content for personal website created using ReactResume

---
+++ README.md
@@ -0,0 +1,37 @@
+# React JS Resume Website Template
+
+![ReactJS Resume Website Template](resume-screenshot.jpg?raw=true 'ReactJS Resume Website Template')
+
+<div align="center">
+
+<img alt="GitHub release (latest by date including pre-releases" src="https://img.shields.io/github/v/release/tbakerx/react-resume-template?include_prereleases">
+
+<img alt="GitHub top language" src="https://img.shields.io/github/languages/top/tbakerx/react-resume-template?style=flat">
+
+<img alt="GitHub Repo forks" src="https://img.shields.io/github/forks/tbakerx/react-resume-template?style=flat&color=success">
+
+<img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/tbakerx/react-resume-template?style=flat&color=yellow">
+
+<img alt="GitHub package.json dependency version (prod)" src="https://img.shields.io/github/package-json/dependency-version/tbakerx/react-resume-template/react?style=flat">
+
+<img alt="Github Repo Sponsors" src="https://img.shields.io/github/sponsors/tbakerx?style=flat&color=blueviolet">
+
+## React based template for software developer-focused resume websites
+
+This is a React based personal resume website template. Built with typescript on the Next.js framework, styled with Tailwind css, and populated with data from a single file, you can easily create, customize and host your own personal website in minutes. Even better, the site is fully mobile-optimized and server-side rendered to ensure fast loading and a clean UI on any device. Read on to learn how to make it your own.
+
+## Project Created & Maintained By
+
+### Tim Baker
+
+<a href="https://twitter.com/timbakerx"><img src="https://github.com/aritraroy/social-icons/blob/master/twitter-icon.png?raw=true" width="60"></a><a href="https://instagram.com/tbakerx"><img src="https://github.com/aritraroy/social-icons/blob/master/instagram-icon.png?raw=true" width="60"></a>
+
+[![GitHub followers](https://img.shields.io/github/followers/tbakerx.svg?style=social&label=Follow)](https://github.com/tbakerx/)
+
+## Stargazers
+
+[![Stargazers repo roster for @tbakerx/react-resume-template](https://reporoster.com/stars/dark/tbakerx/react-resume-template)](https://github.com/tbakerx/react-resume-template/stargazers)
+
+## Forkers
+
+[![Forkers repo roster for @tbakerx/react-resume-template](https://reporoster.com/forks/dark/tbakerx/react-resume-template)](https://github.com/tbakerx/react-resume-template/network/members)
---
+++ src/data/data.tsx
@@ -0,0 +1,450 @@
+import {
+  AcademicCapIcon,
+  ArrowDownTrayIcon,
+  BuildingOffice2Icon,
+  CalendarIcon,
+  FlagIcon,
+  MapIcon,
+  SparklesIcon,
+} from '@heroicons/react/24/outline';
+
+import GithubIcon from '../components/Icon/GithubIcon';
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+import TwitterIcon from '../components/Icon/TwitterIcon';
+import heroImage from '../images/Bukit Batok.webp';
+import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
+import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
+import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+import profilepic from '../images/profilepic.jpg';
+import testimonialImage from '../images/testimonial.jpg';
+import {
+  About,
+  ContactSection,
+  ContactType,
+  Hero,
+  HomepageMeta,
+  PortfolioItem,
+  SkillGroup,
+  Social,
+  TestimonialSection,
+  TimelineItem,
+} from './dataDef';
+
+/**
+ * Page meta data
+ */
+export const homePageMeta: HomepageMeta = {
+  title: 'Aadit Kamat',
+  description: "Aadit Kamat's personal website",
+};
+
+/**
+ * Section definition
+ */
+export const SectionId = {
+  Hero: 'hero',
+  About: 'about',
+  Contact: 'contact',
+  Portfolio: 'portfolio',
+  Resume: 'resume',
+  Skills: 'skills',
+  Stats: 'stats',
+  Testimonials: 'testimonials',
+} as const;
+
+export type SectionId = (typeof SectionId)[keyof typeof SectionId];
+
+/**
+ * Hero section
+ */
+export const heroData: Hero = {
+  imageSrc: heroImage,
+  name: `Hello World! This is Aadit.`,
+  description: (
+    <>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        Welcome to my 🏠 on the internet! <br />
+        I'm a Singapore based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently
+        working with <strong className="text-stone-100">Florida Community Innovation</strong> to build a mobile app that
+        would empower residents within the Miami-Dade county with knowledge about recycleable materials, curbside
+        programs and drop off locations in their vicinity, as well as sustainability related events organized in the
+        area.
+      </p>
+    </>
+  ),
+  actions: [
+    {
+      href: '/assets/resume.pdf',
+      text: 'Resume',
+      primary: true,
+      Icon: ArrowDownTrayIcon,
+    },
+    {
+      href: `#${SectionId.Contact}`,
+      text: 'Contact',
+      primary: false,
+    },
+  ],
+};
+
+/**
+ * About section
+ */
+export const aboutData: About = {
+  profileImageSrc: profilepic,
+  description: `Hey there! I’m Aadit—a problem-solver at heart who loves turning complex challenges into clear, actionable solutions. My journey’s been all about connecting the dots: I started as an engineer, then pivoted to business to understand how technology and strategy intersect.
+
+  I’m the kind of person who thrives in collaborative spaces—whether I’m automating app tests at Shopee, building payment systems at Visa, or helping nonprofits like WeCareJax streamline their operations. What drives me? Seeing how data and systems can create real impact—like designing cash flow models to help struggling businesses or benchmarking restaurant finances to unlock growth.
+
+  Outside of spreadsheets and code, I’m a lifelong learner who believes in growth through curiosity and teamwork.`,
+  aboutItems: [
+    {label: 'Location', text: 'Bukit Batok, Singapore', Icon: MapIcon},
+    {label: 'Age', text: '28', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Singaporean', Icon: FlagIcon},
+    {label: 'Interests', text: 'Effective Altruism, Spirituality, Podcasts, Music (Hindi/English)', Icon: SparklesIcon},
+    {label: 'Study', text: 'University of Florida', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Florida Community Innovation', Icon: BuildingOffice2Icon},
+  ],
+};
+
+/**
+ * Skills section
+ */
+export const skills: SkillGroup[] = [
+  {
+    name: 'Spoken languages',
+    skills: [
+      {
+        name: 'English',
+        level: 10,
+      },
+      {
+        name: 'Hindi',
+        level: 8,
+      },
+      {
+        name: 'Marathi',
+        level: 6,
+      },
+    ],
+  },
+  {
+    name: 'Frontend development',
+    skills: [
+      {
+        name: 'React',
+        level: 9,
+      },
+      {
+        name: 'Typescript',
+        level: 8,
+      },
+      {
+        name: 'GraphQL',
+        level: 6,
+      },
+    ],
+  },
+  {
+    name: 'Backend development',
+    skills: [
+      {
+        name: 'Node.js',
+        level: 9,
+      },
+      {
+        name: 'Spring Boot',
+        level: 6,
+      },
+    ],
+  },
+  {
+    name: 'Mobile development',
+    skills: [
+      {
+        name: 'React Native',
+        level: 9,
+      },
+    ],
+  },
+];
+
+/**
+ * Portfolio section
+ */
+export const portfolioItems: PortfolioItem[] = [
+  {
+    title: 'LEAP Dashboard',
+    description:
+      "Obtained data from LEAP's Monday.com account to create a dashboard showcasing the metrics that highlight the impact that LEAP has created through its programs",
+    url: 'https://dashboard-leap.netlify.app/',
+    image: porfolioImage1,
+  },
+  {
+    title: 'JEA Technology Scorecard',
+    description:
+      "Used ETL to get the data from the original Excel sheet for JEA's employee performance-related metrics into PowerBI and created visuals to help the executives better understand the business metrics",
+    url: 'https://docs.google.com/presentation/d/1yCiyHcRhLLPspg4zvtwjgle7KtiDqY71_t7CVwZrdmA/',
+    image: porfolioImage2,
+  },
+  {
+    title: 'Automating Client Feedback Mechanism - WeCareJax',
+    description:
+      'I created a workflow in Microsoft Power Apps that would allow WeCareJax to  automatically populate their Salesforce dashboard with feedback data from surveys sent to patients post-visit via Microsoft Forms and also investigated ways to improve survey response rates using Mogli, a Salesforce native app that provides bulk messaging capabilities.',
+    url: 'https://drive.google.com/file/d/1ckWJeYl6TEJGMoUiwejcnQj58yc9I6zM/edit?pli=1',
+    image: porfolioImage3,
+  },
+  {
+    title: 'BlueShield Consulting',
+    description:
+      'Conducted market analysis and competitive overview to help BlueShield - a Gainesville based Therapeutics startup - to understand the market for its novel immunotherapy based treatment for Depression',
+    url: 'https://www.science.org/doi/full/10.1126/science.add7150?casa_token=lngchMBZQ2oAAAAA%3AMiX-hHddparHoLFBXhszlQu4YdhiKR9XoqpFa85i6-sZmcDfvJCqDkcfFGwGe79X-89pTHNj6oPZ',
+    image: porfolioImage4,
+  },
+  {
+    title: 'LegalLingo',
+    description: `LegalLingo is a Chrome Extension that allows users to highlight the objectionable sections of their terms of service document and translates the document into different target languages. The aim is to allow users, especially those from minority communities whose primary language is not English, to better understand the documents so that they can make decisions more easily.
+
+    Our goal is to help users, especially those from minority communities, better understand these documents and make more informed decisions.`,
+    url: 'https://github.com/lchin21/Legal-Lingo',
+    image: porfolioImage5,
+  },
+  {
+    title: 'Echo Chat App',
+    description:
+      'Designed and implemented a messaging module for the mobile app to improve user communication, integrating it seamlessly into the platform. Additionally, I improved code quality and streamlined the development process.',
+    url: 'https://github.com/ufosc/Echo-Chat',
+    image: porfolioImage6,
+  },
+  {
+    title: 'Happy Sneeze Market Research Externship',
+    description: 'Give a short description of your project here.',
+    url: 'https://drive.google.com/file/d/1AJldMg0LWAQifHqeJUuyFWCU6q1zxbBF/view',
+    image: porfolioImage7,
+  },
+  {
+    title: 'Comparing Benchmarking Measures for Financial Data',
+    description: `Performed EDA on 6000+ records to establish relationships between different key metrics such as and leveraged Data Envelopment Analysis (DEA) algorithm to benchmark efficiency based on costs and sales, enabling stakeholders to identify underperforming restaurants`,
+    url: 'https://github.com/aaditkamat/QMB6945-RMS-Project',
+    image: porfolioImage8,
+  },
+];
+
+/**
+ * Resume section -- TODO: Standardize resume contact format or offer MDX
+ */
+export const education: TimelineItem[] = [
+  {
+    date: 'December 2024',
+    location: 'Warrington College of Business,University of Florida',
+    title: 'Masters in Information Systems and Operations Research (MSISOM)',
+    content: (
+      <p>
+        As a graduate student under the Data Science track, I learnt how to apply data science techniques to solve
+        real-world business problems. I took courses on databases, data mining, and business intelligence. I also worked
+        on consulting projects with non profit organizations to help them improve their operations and make data-driven
+        decisions
+      </p>
+    ),
+  },
+  {
+    date: 'June 2021',
+    location: 'School of Computing, National University of Singapore',
+    title: 'Bachelor of Computing (BComp. Honours) in Computer Science ',
+    content: (
+      <p>
+        This program gave me a foundation for all my technical explorations. I not only learnt software engineering
+        skills but also the fundamentals underlying computer systems like OS, Computer Architecture in addition to
+        taking electives in AI and Machine Learning.
+      </p>
+    ),
+  },
+];
+
+export const experience: TimelineItem[] = [
+  {
+    date: 'April 2024 - Present',
+    location: 'Florida Community Innovation',
+    title: 'Lead Software Engineer',
+    content: (
+      <>
+        <p>
+          I am currently leading the development of a mobile app called Recyclepedia that Florida Community Innovation
+          (FCI) is building in partnership with Dream in Green (DiG) to empower residents of the Miami Dade county area
+          - which has one of the lowest recycling rates within the state - with the knoweldge that would help them adopt
+          better recycling practices. <br />
+        </p>
+        <br />
+        <p>Some of the features planned for the mobile app as part of its roadmap:</p>
+        <ol>
+          <li>
+            The home page with a calendar of community events related to recycling organized by Dream in Green (with an
+            ad space to allow sponsors to air environmental sustainability related content that helps create revenue for
+            the app while keeping it free for users){' '}
+          </li>
+          <li>
+            Curbside/Drop off page that allows users to select the categories of items they wish to recycle and suggest
+            them the nearest curbside pickup/drop off location(s) where they can keep their recyclable materials{' '}
+          </li>
+          <li>Barcode scanner that provides information about whether the material is recyclable or not</li>
+          <li>
+            Authentication that allows users to create accounts so that they can specifically keep track of which events
+            they are attending as well as points and rewards earned from games
+          </li>
+          <li>
+            Chatbot that answers questions from users about recycling and other environmental sustainability initiatives
+          </li>
+          <li>Mobile games that highlight effective recycling practices</li>
+        </ol>
+      </>
+    ),
+  },
+  {
+    date: 'June 2024 - August 2024',
+    location: 'Getzler Henrich & Associates LLC',
+    title: 'Data Analyst Intern',
+    content: (
+      <>
+        <p>
+          I teamed up with senior analysts to create a customized 13-week cash flow model, which improved our
+          forecasting accuracy and helped with strategic planning. I also put together a detailed data dictionary for an
+          Excel model to improve data consistency and accessibility.
+        </p>
+        <br />
+        <p>
+          Using Microsoft Fabric, I prototyped a data pipeline to process client financial data from ERP systems. After
+          data cleaning, ingestion, and processing, the pipeline creates a 13-week cash flow model with 80% forecasting
+          accuracy, reducing FP&A analysis time by 30 hours/quarter.
+        </p>
+      </>
+    ),
+  },
+  {
+    date: 'February 2024 - May 2024',
+    location: 'J. Crayton Pruitt Family Department of Biomedical Engineering, University of Florida',
+    title: 'Graduate Research Assistant',
+    content: (
+      <p>
+        I collaborated with Dr. Xiao Fan - the head of the Fan Lab at the University of Florida - on research
+        investigating the relationship between variant pathogenicity and protein characteristics. I conducted
+        bioinformatics analysis and interpreted the results of the analysis, enabling publication of an analysis on the
+        Pediatrics Cardiomyopathy Genes Study
+      </p>
+    ),
+  },
+  {
+    date: 'March 2022 - September 2022',
+    location: 'Visa Worldwide Pte Ltd',
+    title: 'Full Stack Software Engineer',
+    content: (
+      <>
+        <p>
+          I engineered the backend APIs within a microservices based architecture used by major India banks with over
+          100 thousand customers enrolled with VisaNet Accounts Based Payment Solutions.
+        </p>
+        <br />
+        <p>
+          I also built the frontend interface for an internal portal that streamlined the process of dynamically
+          accessing and manipulating data used by mobile apps in the Singapore Innovation Centre, demonstrating
+          real-time payment processing capabilities to clients.
+        </p>
+      </>
+    ),
+  },
+  {
+    date: 'June 2024 - August 2024',
+    location: 'Shopee Singapore Pte Ltd',
+    title: 'Quality Assurance Engineer',
+    content: (
+      <>
+        <p>
+          I helped develop and maintain over 100 test cases for the Promotion modules (Flash Sale/Bundle Deal/Add on
+          Deal/Discount) for the Android App, significantly enhancing the user experience for more than a million
+          customers.
+        </p>
+        <br />
+        <p>
+          I also worked closely with QA Engineers from other departments to refresh our training syllabus and guide new
+          engineers in automation tools like Espresso and UiAutomator.
+        </p>
+      </>
+    ),
+  },
+];
+
+/**
+ * Testimonial section
+ */
+export const testimonial: TestimonialSection = {
+  imageSrc: testimonialImage,
+  testimonials: [
+    {
+      name: 'Caroline Nickerson',
+      text: `Aadit is the best! Not only does he have technical know-how, but he is also kind, considerate, and a great mentor to younger students. He’s currently building an app that will improve recycling education in Florida, and he’s deftly able to communicate with expert and nonexpert audiences to create tech that serves social good. Anyone would be lucky to work with Aadit!`,
+      image:
+        'https://media.licdn.com/dms/image/v2/C5603AQH9_myyvM332A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1626871195696?e=1753920000&v=beta&t=PGSv4DqQdbOKaIXyu4IMGmVx3vCy5N8lJJt4WXbtZ-w',
+    },
+    {
+      name: 'Xiao Fan',
+      text: `Aadit is an independent, self-disciplined, diligent student. When he expressed a strong interest in pursuing data science projects, I introduced him to a bioinformatics project. Despite not having formal training in biology, he quickly grasped the main concepts of the project and data representation of the biology entities. Showing great enthusiasm, Aadit thoroughly explored the datasets and successfully extracted the required information for his project. I am confident that Aadit will excel in his career pursuits and make meaningful contributions to his chosen field of study.`,
+      image:
+        'https://media.licdn.com/dms/image/v2/C4D03AQFKBnZAEnGmAQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1526922137987?e=1753920000&v=beta&t=jTdMq-5QMSGipM-JFVd4NqGaFRFh4JZEdCgBsIqgZ-s',
+    },
+    {
+      name: 'Shwetha Ravi',
+      text: 'Aadit is a talented software engineer and picks up new skills at an impressive rate. He is also responsible, communicative and passionate about self development. Would highly recommend having him on your team.',
+      image:
+        'https://media.licdn.com/dms/image/v2/D5603AQGUyWeNIALDxQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1686740577700?e=1753920000&v=beta&t=4ISXB0Vi5Eor863Q62IIiUgvX_S7HcACOavv6WhF9HQ',
+    },
+    {
+      name: 'Robin Loh',
+      text: `I was Aadit's mentor during NUS Orbital 2018, which is a self-directed module offered every summer by NUS School of Computing. Together with his teammate, both of them worked on the PlanItRight project, which is a web application that plans itineraries based on the user's travel plans and preferences.
+      Aadit demonstrated great enthusiasm in learning software development skills relevant to his project. He successfully defined the functionalities suitable for the intended users in this project and he crafted simple user and developer guides for the web application. There were also many clear and useful illustrations implemented in the web application, such as the use of timeline to visualise the itineraries in a chronological order.
+      Throughout the 3 months of mentorship, he displayed good analytical and interpersonal skills. He comprehended new concepts very quickly and he structured his approaches clearly to accomplish his outcomes. I strongly believe that he has the capability to work independently on tough challenges given his inquisitiveness for self-improvement and for developing his technical expertise further.`,
+      image:
+        'https://media.licdn.com/dms/image/v2/C5603AQGLf9kumkR6Xw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1590974862235?e=1753920000&v=beta&t=ro_lYyuNA9Tlod9z4S9GTgRBf8xXZPDBy4kvzxo_als',
+    },
+  ],
+};
+
+/**
+ * Contact section
+ */
+
+export const contact: ContactSection = {
+  headerText: 'Get in touch.',
+  description: 'Hit me up for career and project collaboration opportunities, or if you just want to have a chat 😊',
+  items: [
+    {
+      type: ContactType.Email,
+      text: 'aadit.kamat@floridainnovation.org',
+      href: 'mailto:aadit.kamat@floridainnovation.org',
+    },
+    {
+      type: ContactType.Location,
+      text: 'Bukit Batok, SG',
+      href: 'https://www.google.com/maps/place/Bukit+Batok+Street+25,+Block+289A,+Singapore/@1.344666,103.75792,17z',
+    },
+    {
+      type: ContactType.Github,
+      text: '@aaditkamat',
+      href: 'https://github.com/aaditkamat',
+    },
+  ],
+};
+
+/**
+ * Social items
+ */
+export const socialLinks: Social[] = [
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/aaditkamat'},
+  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/10334485/aadit-kamat'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/aaditkamat/'},
+  {label: 'X', Icon: TwitterIcon, href: 'https://x.com/aaditkamat'},
+];
