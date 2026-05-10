d862332d644c8c38a9a7072e77a5431a415ebd4f
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2026-05-10 14:45:02 +0800

    docs: update portfolio images

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -20,7 +20,7 @@ const Portfolio: FC = memo(() => {
           {portfolioItems.map((item, index) => {
             const {title, image} = item;
             return (
-              <div className="pb-6" key={`${title}-${index}`}>
+              <div className="py-6" key={`${title}-${index}`}>
                 <div
                   className={classNames(
                     'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
