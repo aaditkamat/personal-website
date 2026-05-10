6b2405d25682b0567d304591fde9225928b7577b
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2026-05-10 19:33:52 +0800

    fix: update assets and page reference

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  siteUrl: 'reactresume.com',
+  siteUrl: 'aaditkamat.dev',
   exclude: ['/404*', '/500*'],
   transform: async (config, path) => {
     return {
