25cbe92fe26b0b674fb55c6b74e8967d71d466a8
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2026-05-10 19:33:52 +0800

    fix: update assets and page reference

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -23,18 +23,12 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
 
         <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="/site.webmanifest" rel="manifest" />
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
         <meta content={`https://reactresume.com${pathname}`} property="og:url" />
-
-        {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
-        <meta content={title} name="twitter:title" />
-        <meta content={description} name="twitter:description" />
       </Head>
       <Script dangerouslySetInnerHTML={CHATBASE_DANGEROUS_HTML} id="chatbase-snippet" strategy="afterInteractive" />
       {children}
