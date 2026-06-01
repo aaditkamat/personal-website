aec816cd3746e826cc990af4caa6ac270b04af74
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2026-06-01 10:37:02 +0800

    feat: add cloudflare turnstile

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,17 +1,17 @@
-import {NextPage} from 'next';
+import { NextPage } from 'next';
 import Head from 'next/head';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 import Script from 'next/script';
-import {memo, PropsWithChildren} from 'react';
+import { memo, PropsWithChildren } from 'react';
 
-import {HomepageMeta} from '../../data/dataDef';
+import { HomepageMeta } from '../../data/dataDef';
 
 const CHATBASE_SNIPPET = `(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase = (...arguments) => { if (!window.chatbase.q) { window.chatbase.q = [] } window.chatbase.q.push(arguments) };window.chatbase=new Proxy(window.chatbase,{get(target, prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="XOb4KSWDUZwjZCiWH9FL4";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load", onLoad)}})();`;
 
-const CHATBASE_DANGEROUS_HTML = {__html: CHATBASE_SNIPPET};
+const CHATBASE_DANGEROUS_HTML = { __html: CHATBASE_SNIPPET };
 
-const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
-  const {asPath: pathname} = useRouter();
+const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({ children, title, description }) => {
+  const { asPath: pathname } = useRouter();
 
   return (
     <>
@@ -24,6 +24,7 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
 
         <link href="/favicon.ico" rel="icon" sizes="any" />
         <link href="/site.webmanifest" rel="manifest" />
+        <link rel="preconnect" href="https://challenges.cloudflare.com" />
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
@@ -31,6 +32,7 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <meta content={`https://www.aaditkamat.dev${pathname}`} property="og:url" />
       </Head>
       <Script dangerouslySetInnerHTML={CHATBASE_DANGEROUS_HTML} id="chatbase-snippet" strategy="afterInteractive" />
+      <Script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer />
       {children}
     </>
   );
